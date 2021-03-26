b198e274e00a7e8b8c348802e27016ba550d73b7
Arthur Borba <artsborba@gmail.com>
2021-03-26 14:43:16 -0300

    package.json

---
+++ src/time-range-picker.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
