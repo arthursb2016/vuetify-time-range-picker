b027b9a76a0b0513f553ec5e6fe65b264fb61287
Arthur Borba <artsborba@gmail.com>
2021-03-29 11:32:28 -0300

    load vue first

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,18 +2,18 @@ import commonjs from '@rollup/plugin-commonjs';
 import vue from 'rollup-plugin-vue';
 import buble from '@rollup/plugin-buble';
 
-export default [{
+export default {
   input: 'src/index.js',
   output: {
     name: 'TimeRangePicker',
     exports: 'named',
   },
   plugins: [
-    commonjs(),
     vue({
         css: true,
         compileTemplate: true,
     }),
+    commonjs(),
     buble(),
   ],
-}];
+};
