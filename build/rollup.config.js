be411cca96f39f0f5af669ccfb80be96a18f3fae
Arthur Borba <artsborba@gmail.com>
2021-04-19 18:48:03 -0300

    im getting there

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,7 +1,11 @@
 import commonjs from '@rollup/plugin-commonjs';
 import vue from 'rollup-plugin-vue';
 import buble from '@rollup/plugin-buble';
+import sass from 'rollup-plugin-sass';
 import scss from 'rollup-plugin-scss';
+import postcss from 'rollup-plugin-postcss';
+import vuetify from 'rollup-plugin-vuetify';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
@@ -9,13 +13,17 @@ export default {
     name: 'TimeRangePicker',
     exports: 'named',
   },
+  external: ['vue', 'vuetify/lib'],
   plugins: [
-    vue({
-        css: true,
-        compileTemplate: true,
+    postcss(),
+    nodeResolve(),
+    commonjs({
+      include: 'node_modules',
     }),
     scss(),
-    commonjs(),
+    sass(),
+    vue(),
+    vuetify(),
     buble({
       objectAssign: 'Object.assign',
     }),
